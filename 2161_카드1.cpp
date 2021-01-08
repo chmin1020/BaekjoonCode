331d68564f5d1c51a3ad569b86435ba7c251f452
germanJM <chmin1020@gmail.com>
2021-01-08 19:44:38 +0900

    Create 2161_카드1.cpp
    completed in 2021.01.07

---
+++ 2161_카드1.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <queue>
+using namespace std;
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL); cout.tie(NULL);
+	queue<int> qu;
+	int n;
+	bool repush = false;
+	
+	cin >> n;
+	for (int i = 1; i <= n; i++) qu.push(i);
+	while (qu.size() > 1) {
+		if (repush) qu.push(qu.front());
+		else cout << qu.front() << " ";
+		qu.pop(); repush = !repush;
+	}
+	cout << qu.front() << '\n';
+	return 0;
+}

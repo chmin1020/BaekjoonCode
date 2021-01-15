b42ca0b6dfc36316043f628b1ed6403040ee8f1d
germanJM <chmin1020@gmail.com>
2021-01-15 19:26:45 +0900

    Update 1697_숨바꼭질.cpp
    better code than before

--- 1000~9999/1697_숨바꼭질.cpp
+++ 1000~9999/1697_숨바꼭질.cpp
@@ -4,15 +4,10 @@
 using namespace std;
 bool visited[100001];
 
-bool push_check(queue<pair<int,int>> &q, int n, int target) {
-	if (n == target) {
-		cout << q.front().second + 1 << '\n';
-		return true;
-	}
+void push_check(queue<pair<int,int>> &q, int n, int target) {
 	if (n >= 0 && n <= 100000 && !visited[n]) {
 		q.push(make_pair(n, q.front().second + 1)); visited[n] = true;
 	}
-	return false;
 }
 
 int main() {
@@ -23,15 +18,14 @@ int main() {
 	int cur, tar;
 
 	cin >> cur >> tar;
-	if (cur == tar) cout << "0\n";
-	else {
-		bfs.push(make_pair(cur, 0)); visited[cur] = true;
-		while (1) {
-			if (push_check(bfs, bfs.front().first - 1, tar)) break;
-			if (push_check(bfs, bfs.front().first + 1, tar)) break;
-			if (push_check(bfs, bfs.front().first * 2, tar)) break;
-			bfs.pop();
-		}
+	bfs.push(make_pair(cur, 0)); visited[cur] = true;
+	while (1) {
+		if (bfs.front().first == tar) break;
+		push_check(bfs, bfs.front().first - 1, tar);
+		push_check(bfs, bfs.front().first + 1, tar);
+		push_check(bfs, bfs.front().first * 2, tar);
+		bfs.pop();
 	}
+	cout << bfs.front().second << '\n';
 	return 0;
 }

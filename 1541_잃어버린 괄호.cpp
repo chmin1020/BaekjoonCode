30f9cbcd1570fbdc80cba0fc9d0ad4920e80e474
germanJM <chmin1020@gmail.com>
2021-01-17 23:05:32 +0900

    Create 1541_잃어버린 괄호.cpp
    completed in 2020.08.18

---
+++ 1541_잃어버린 괄호.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
+
+int main() {
+	string folmula;
+	vector<int> num;
+	vector<char> op;
+	vector<int> tp;
+
+	cin >> folmula;
+
+	//숫자와 연산자 분리
+	int tmp = 0;
+	for (int i = 0; i < (signed int)folmula.size(); i++) {
+
+		if (folmula[i] == '+' || folmula[i] == '-') {
+			num.push_back(stoi(folmula.substr(i - tmp, tmp)));
+			tmp = 0;
+			op.push_back(folmula[i]);
+		}
+		else if (i == folmula.size() - 1) 
+			num.push_back(stoi(folmula.substr(i - tmp, tmp + 1)));
+		else tmp++;
+	}
+
+	//더하기 먼저
+	int idx = 0;
+	tp.push_back(num[0]);
+	for (int i = 1; i < num.size(); i++) {
+		if (op[i - 1] == '+')
+			tp[idx] += num[i];
+		else {
+			idx++;
+			tp.push_back(num[i]);
+		}
+	}
+
+	//빼기 시행 후 결과 출력
+	int result = tp[0];
+	for (int i = 1; i < tp.size(); i++)
+		result -= tp[i];
+	cout << result << '\n';
+
+
+
+	return 0;
+}

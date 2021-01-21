667918140ee4de78aa5e0577a3dd2503af91517e
germanJM <chmin1020@gmail.com>
2021-01-21 22:26:01 +0900

    Update 10026_적록색약.cpp

--- 10000~/10026_적록색약.cpp
+++ 10000~/10026_적록색약.cpp
@@ -6,7 +6,7 @@ using namespace std;
 char pic[101][101];
 bool visited[101][101];
 
-int res(int size, bool weak) {
+int res(int size) {
 	queue<pair<pair<int, int>, char>> bfs;
 	memset(visited, false, sizeof(visited));
 	int tpx, tpy, cnt = 0;
@@ -19,29 +19,21 @@ int res(int size, bool weak) {
 				do {
 					tpx = bfs.front().first.first; tpy = bfs.front().first.second;
 
-					if (tpx > 1 && !visited[tpx - 1][tpy]) {
-						if ((weak && ((pic[tpx - 1][tpy] != 'B' && bfs.front().second != 'B') || (pic[tpx - 1][tpy] == 'B' && bfs.front().second == 'B'))) || (!weak && pic[tpx - 1][tpy] == bfs.front().second)) {
-							bfs.push(make_pair(make_pair(tpx - 1, tpy), pic[tpx - 1][tpy]));
-							visited[tpx - 1][tpy] = true;
-						}
+					if (tpx > 1 && !visited[tpx - 1][tpy] && pic[tpx - 1][tpy] == bfs.front().second) {
+						bfs.push(make_pair(make_pair(tpx - 1, tpy), pic[tpx - 1][tpy]));
+						visited[tpx - 1][tpy] = true;
 					}
-					if (tpx < size && !visited[tpx + 1][tpy]) {
-						if ((weak && ((pic[tpx + 1][tpy] != 'B' && bfs.front().second != 'B') || (pic[tpx + 1][tpy] == 'B' && bfs.front().second == 'B'))) || (!weak && pic[tpx + 1][tpy] == bfs.front().second)) {
-							bfs.push(make_pair(make_pair(tpx + 1, tpy), pic[tpx + 1][tpy]));
-							visited[tpx + 1][tpy] = true;
-						}
+					if (tpx < size && !visited[tpx + 1][tpy] && pic[tpx + 1][tpy] == bfs.front().second) {
+						bfs.push(make_pair(make_pair(tpx + 1, tpy), pic[tpx + 1][tpy]));
+						visited[tpx + 1][tpy] = true;
 					}
-					if (tpy > 1 && !visited[tpx][tpy - 1]) {
-						if ((weak && ((pic[tpx][tpy - 1] != 'B' && bfs.front().second != 'B') || (pic[tpx][tpy - 1] == 'B' && bfs.front().second == 'B'))) || (!weak && pic[tpx][tpy - 1] == bfs.front().second)) {
-							bfs.push(make_pair(make_pair(tpx, tpy - 1), pic[tpx][tpy - 1]));
-							visited[tpx][tpy - 1] = true;
-						}
+					if (tpy > 1 && !visited[tpx][tpy - 1] && pic[tpx][tpy - 1] == bfs.front().second) {
+						bfs.push(make_pair(make_pair(tpx, tpy - 1), pic[tpx][tpy - 1]));
+						visited[tpx][tpy - 1] = true;
 					}
-					if (tpy < size && !visited[tpx][tpy + 1]) {
-						if ((weak && ((pic[tpx][tpy + 1] != 'B' && bfs.front().second != 'B') || (pic[tpx][tpy + 1] == 'B' && bfs.front().second == 'B'))) || (!weak && pic[tpx][tpy + 1] == bfs.front().second)) {
-							bfs.push(make_pair(make_pair(tpx, tpy + 1), pic[tpx][tpy + 1]));
-							visited[tpx][tpy + 1] = true;
-						}
+					if (tpy < size && !visited[tpx][tpy + 1] && pic[tpx][tpy + 1] == bfs.front().second) {
+						bfs.push(make_pair(make_pair(tpx, tpy + 1), pic[tpx][tpy + 1]));
+						visited[tpx][tpy + 1] = true;
 					}
 					bfs.pop();
 				} while (!bfs.empty());
@@ -63,6 +55,10 @@ int main() {
 		for (int j = 1; j <= size; j++)
 			pic[i][j] = input[j - 1];
 	}
-	cout << res(size, false) << ' ' << res(size, true) << '\n';
+	cout << res(size) << ' ';
+	for (int i = 1; i <= size; i++) 
+		for (int j = 1; j <= size; j++)
+			if (pic[i][j] == 'G') pic[i][j] = 'R';
+	cout << res(size) << '\n';
 	return 0;
 }

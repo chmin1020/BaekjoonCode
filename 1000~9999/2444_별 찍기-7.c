a55c79abe001b14c013bacf06751720520b78967
germanJM <chmin1020@gmail.com>
2021-02-22 22:36:47 +0900

    Create 2444_별 찍기-7.c
    completed in 2020.03.24

---
+++ 1000~9999/2444_별 찍기-7.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main()
+{
+	int in;
+	scanf("%d", &in);
+
+	for (int i = 0; i < in; i++)
+	{
+		for (int j = 0; j < in - 1 - i; j++)
+			printf(" ");
+		for (int k = 0; k < 2 * i + 1; k++)
+			printf("*");
+		printf("\n");
+	}
+	for (int i = 1; i < in; i++)
+	{
+		for (int j = 0; j < i; j++)
+			printf(" ");
+		for (int k = 0; k < 2 * (in - i) - 1; k++)
+			printf("*");
+		printf("\n");
+	}
+
+	return 0;
+}

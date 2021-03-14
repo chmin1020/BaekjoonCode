918359f0af073e7a51a82179ce6e03af4925d104
germanJM <chmin1020@gmail.com>
2021-03-14 23:28:19 +0900

    Create 1915_가장 큰 정사각형.cpp
    completed in 2021.03.14

---
+++ 1000~9999/1915_가장 큰 정사각형.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <cstring>
+#include <algorithm>
+using namespace std;
+int board[1001][1001];
+
+void dynamic(int n, int m) {
+	int dp[1001][1001];
+	int ans = 0;
+
+	memset(dp, 0, sizeof(dp));
+	for (int i = 1; i <= n; i++) {
+		for (int j = 1; j <= m; j++) {
+			if (board[i][j] == 1)
+				dp[i][j] = min(min(dp[i - 1][j], dp[i][j - 1]), dp[i - 1][j - 1]) + 1;
+			ans = max(ans, dp[i][j]);
+		}
+	}
+	cout << ans*ans << "\n";
+}
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL); cout.tie(NULL);
+	int n, m;
+	char tmp[1001];
+
+	cin >> n >> m;
+	for (int i = 1; i <= n; i++) {
+		scanf("%s", tmp);
+		for (int j = 0; j < m; j++)
+			board[i][j + 1] = tmp[j] - '0';
+	}
+	dynamic(n, m);
+	return 0;
+}

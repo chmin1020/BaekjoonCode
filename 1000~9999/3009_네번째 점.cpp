94ad4d088b525add234136e8d05729e294a7e67f
germanJM <chmin1020@gmail.com>
2021-03-22 00:06:36 +0900

    Create 3009_네번째 점.cpp
    completed in 2021.03.22

---
+++ 1000~9999/3009_네번째 점.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+int main() {
+	int x1, x2, x3, y1, y2, y3;
+
+	std::cin >> x1 >> y1;
+	std::cin >> x2 >> y2;
+	std::cin >> x3 >> y3;
+
+	if (x1 != x2 && x1 != x3) std::cout << x1;
+	else if (x2 != x1 && x2 != x3) std::cout << x2;
+	else std::cout << x3;
+	putc(' ', stdout);
+	if (y1 != y2 && y1 != y3) std::cout << y1;
+	else if (y2 != y1 && y2 != y3) std::cout << y2;
+	else std::cout << y3;
+	putc('\n', stdout);
+	return 0;
+}

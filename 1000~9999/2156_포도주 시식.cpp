8707a2fffce3c7b682e7509239079e64fb5bd6da
germanJM <chmin1020@gmail.com>
2021-05-07 23:50:35 +0900

    Update 2156_포도주 시식.cpp

--- 1000~9999/2156_포도주 시식.cpp
+++ 1000~9999/2156_포도주 시식.cpp
@@ -7,20 +7,21 @@ int dp[10001];
 
 int find(int n) {
 	if (n < 0) return 0;
-	if (dp[n] == 0) {
+	if (dp[n] == -1) {
 		dp[n] = max(wine[n] + wine[n - 1] + find(n - 3), wine[n] + find(n - 2));
-		if (dp[n] < wine[n-1]+wine[n-2]+find(n-4)) dp[n] = wine[n - 1] + wine[n - 2] + find(n - 4);
+		if (dp[n] < wine[n - 1] + wine[n - 2] + find(n - 4)) dp[n] = wine[n - 1] + wine[n - 2] + find(n - 4);
 	}
 	return dp[n];
 }
 
 int main() {
-	memset(dp, 0, sizeof(dp));
 	int n;
 	cin >> n;
-	
-	for (int i = 1; i <= n; i++)
+
+	for (int i = 1; i <= n; i++) {
 		cin >> wine[i];
+		dp[i] = -1;
+	}
 	dp[1] = wine[1];
 	cout << find(n) << '\n';
 	return 0;

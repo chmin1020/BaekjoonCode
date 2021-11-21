eeb73fc72976024640cf5713c5a0b5783e36789d
germanJM <chmin1020@gmail.com>
2021-11-21 21:54:46 +0900

    Update 13549_숨바꼭질3.java
    change to 0-1 bfs

--- 10000~/13549_숨바꼭질3.java
+++ 10000~/13549_숨바꼭질3.java
@@ -4,51 +4,44 @@ import java.util.*;
 
 public class Main {
 	private static final int LEN = 100001;
-	private static boolean[] visited = new boolean[LEN];
-	
-	public static class Pair implements Comparable<Pair>{
-		public int pos, time;
-		Pair(int pos, int time){
-			this.pos= pos;
-			this.time = time;
-		}
-		public int compareTo(Pair o) {
-			return this.time > o.time ? 1 : -1;
-		}
-	}
+	private static final int INF = 50000000;
+	private static int[] dis = new int[LEN];
 	
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 		StringTokenizer st;
-		PriorityQueue<Pair> qu = new PriorityQueue<Pair>();
-		int n, k;
-		Pair cur = null;
+		Deque<Integer> qu = new LinkedList<Integer>();
+		int n, k, cur;
 		
-		Arrays.fill(visited, false);
+		Arrays.fill(dis, INF);
 		st = new StringTokenizer(br.readLine());
 		n = Integer.parseInt(st.nextToken());
 		k = Integer.parseInt(st.nextToken());
 		
-		qu.add(new Pair(n, 0));
-		
+		qu.add(n);
+		dis[n] = 0;
 		while(!qu.isEmpty()) {
-			cur = qu.poll();
-			if(visited[cur.pos]) continue;
-			if(cur.pos == k) {
-				bw.write(cur.time + "\n");
+			cur = qu.pollFirst();
+			if(cur == k) {
+				bw.write(dis[cur] + "\n");
 				break;
 			}
-			visited[cur.pos] = true;
 				
-			if(cur.pos * 2 < LEN && !visited[cur.pos * 2]) 
-				qu.add(new Pair(cur.pos * 2, cur.time));
-			
-			if(cur.pos - 1 >= 0 && !visited[cur.pos - 1]) 
-				qu.add(new Pair(cur.pos - 1, cur.time + 1));
+			if(cur * 2 < LEN && dis[cur * 2] > dis[cur]) {	
+				dis[cur * 2] = dis[cur];
+				qu.addFirst(cur * 2);	
+			}
 
-			if(cur.pos + 1 < LEN && !visited[cur.pos + 1]) 
-				qu.add(new Pair(cur.pos + 1, cur.time + 1));
+			if(cur + 1 < LEN && dis[cur + 1] > dis[cur] + 1) {			
+				dis[cur + 1] = dis[cur] + 1;
+				qu.addLast(cur + 1);
+			}
+			
+			if(cur - 1 >= 0 && dis[cur - 1] > dis[cur] + 1) { 
+				dis[cur - 1] = dis[cur] + 1;
+				qu.addLast(cur - 1);		
+			}
 		}
 		bw.flush();
 		bw.close();

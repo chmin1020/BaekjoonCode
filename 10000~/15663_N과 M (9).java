e2b0a3a72d2e3fc31cc96932552ffc3772baf5de
germanJM <chmin1020@gmail.com>
2021-12-23 13:52:18 +0900

    Create 15663_N과 M (9).java
    completed in 2021.12.23

---
+++ 10000~/15663_N과 M (9).java
@@ -0,0 +1,58 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	static int n, m;
+	static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+	static int prev = -1;
+	static boolean[] visited;
+	static int[] seq, ans;
+	
+	public static void backTracking(int s, int num) throws IOException{
+		ans[num] = seq[s];	
+		if(num == m - 1) {
+			for(int i = 0; i < m; i++) 
+				bw.write(ans[i] + " ");
+			bw.write("\n");
+			return;
+		}
+		visited[s] = true;
+		for(int i = 0; i < n; i++) {
+			if(!visited[i] && seq[i] != prev) {
+				prev = -1;
+				backTracking(i, num + 1);
+				prev = seq[i];
+			}
+		}
+		visited[s] = false;
+	}
+	
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+		
+		st = new StringTokenizer(br.readLine());
+		n = Integer.parseInt(st.nextToken());
+		m = Integer.parseInt(st.nextToken());
+		seq = new int[n];
+		ans = new int[m];
+		visited = new boolean[n];
+		Arrays.fill(visited, false);
+		
+		st = new StringTokenizer(br.readLine());
+		for(int i = 0; i < n; i++)
+			seq[i] = Integer.parseInt(st.nextToken());
+		Arrays.sort(seq);
+		
+		for(int i = 0; i < n; i++) {
+			if(prev != seq[i]) {
+				prev = -1;
+				backTracking(i, 0);
+				prev = seq[i];
+			}
+		}		
+		bw.flush();
+		bw.close();
+		br.close();
+	}
+}
